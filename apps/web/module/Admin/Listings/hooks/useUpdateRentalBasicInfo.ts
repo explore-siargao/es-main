0012773506f9f89bb9e94c205df5e1a0193deda6
Alkaejah <richard.delapena19@gmail.com>
2024-06-05 10:59:40 +0800

    feat(web): Implement Update Rental Details

--- apps/web/module/Admin/Listings/hooks/useUpdateRentalBasicInfo.ts
+++ apps/web/module/Admin/Listings/hooks/useUpdateRentalBasicInfo.ts
@@ -4,14 +4,14 @@ import { useMutation } from "@tanstack/react-query"
 import { T_Rental_Basic_Info } from "@repo/contract"
 
 export async function updateRentalBasicInfo(
-  id: number | undefined,
+  id: string | undefined,
   props: T_Rental_Basic_Info
 ) {
   const apiService = new ApiService("mock")
   return await apiService.patch(`${API_URL_RENTALS}/${id}/basic-info`, props)
 }
 
-function useUpdateRentalBasicInfo(id: number | undefined) {
+function useUpdateRentalBasicInfo(id: string | undefined) {
   const query = useMutation({
     mutationFn: (props: T_Rental_Basic_Info) =>
       updateRentalBasicInfo(id, props),
