b5286e70324a9eb6918085c1df6062acdf494700
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-06-05 15:29:31 +0800

    fix: Fixed sonar cloud issue

--- apps/web/module/Hosting/Listings/Rentals/Rental/hooks/useUpdateRentalFinshedSection.ts
+++ apps/web/module/Hosting/Listings/Rentals/Rental/hooks/useUpdateRentalFinshedSection.ts
@@ -1,14 +1,13 @@
 import { API_URL_RENTALS } from "@/common/constants"
 import { ApiService } from "@/common/service/api"
-import { T_Listing_Location } from "@repo/contract"
 import { useMutation } from "@tanstack/react-query"
 
-interface I_RentalFinishedSection {
+interface IRentalFinishedSection {
   finishedSection: string[]
 }
 export async function updateRentalFinishedSection(
   id: string | undefined,
-  props: I_RentalFinishedSection
+  props: IRentalFinishedSection
 ) {
   const apiService = new ApiService("v2")
   return await apiService.patch(
@@ -19,7 +18,7 @@ export async function updateRentalFinishedSection(
 
 function useUpdateRentalFinishedSection(id: string | undefined) {
   const query = useMutation({
-    mutationFn: (props: I_RentalFinishedSection) =>
+    mutationFn: (props: IRentalFinishedSection) =>
       updateRentalFinishedSection(id, props),
   })
   return query
