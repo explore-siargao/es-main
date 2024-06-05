3bb9c5e2149cde481adb00b0b6c96a30d94eefc9
Alkaejah <richard.delapena19@gmail.com>
2024-06-05 16:39:55 +0800

    feat(web): Implement Edit rental photo info api v2

--- apps/web/module/Admin/Listings/Declined/Rental/hooks/useUpdateRentalPhotos.ts
+++ apps/web/module/Admin/Listings/Declined/Rental/hooks/useUpdateRentalPhotos.ts
@@ -3,10 +3,7 @@ import { API_URL_RENTALS } from "@/common/constants"
 import { useMutation } from "@tanstack/react-query"
 import { T_Photo } from "@repo/contract"
 
-export async function updateRentalPhotos(
-  id: string | undefined,
-  props: { photos: T_Photo[] }
-) {
+export async function updateRentalPhotos(id: string | undefined,props: { photos: T_Photo[]}) {
   const apiService = new ApiService("v2")
   return await apiService.patch(`${API_URL_RENTALS}/${id}/photos`, props)
 }
