cbfd9f670ec09821fa1436d6701fbf66759bc237
Andal <arjayandal93@gmail.com>
2024-06-06 09:29:10 +0800

    feat(web): Created isHostActivityOwner middleware v2

---
+++ apps/api/src/routes/api/activities-v2/middleware/isHostActivityOwner.ts
@@ -0,0 +1,48 @@
+// NOTE: This needs to be place after isUserLoggedIn middleware
+
+import { NextFunction, Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { ResponseService } from '@/common/service/response';
+import { USER_NOT_AUTHORIZED } from '@/common/constants';
+
+const response = new ResponseService();
+
+const isHostActivityOwner = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const loggedInUser = res.locals.user;
+  const activityId = req.params.activityId;
+
+  if (loggedInUser.role === 'Admin') {
+    return next();
+  }
+
+  let activity;
+  try {
+    //@ts-expect-error
+    activity = await dbActivities.findOne({
+      hostId: loggedInUser.id,
+      _id: activityId
+    });
+  } catch (err) {
+    return res.status(400).json(
+      response.error({
+        message: 'Invalid activity ID format.',
+      })
+    );
+  }
+
+  if (activity && loggedInUser.isHost) {
+    return next();
+  } else {
+    return res.status(403).json(
+      response.error({
+        message: USER_NOT_AUTHORIZED,
+      })
+    );
+  }
+};
+
+export default isHostActivityOwner;
