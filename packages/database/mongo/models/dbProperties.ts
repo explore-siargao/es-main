1ab8f1fe2f983b8b50a0d01f0bc0ac67ccd9168f
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-06-18 08:29:09 +0800

    feat(api): Converted create whole place unit endpoint to api v2
    Prettified Code!

--- packages/database/mongo/models/dbProperties.ts
+++ packages/database/mongo/models/dbProperties.ts
@@ -71,7 +71,7 @@ const properties = new Schema({
   bookableUnits: [
     {
       type: mongoose.Schema.ObjectId,
-      ref: "BookableUnits",
+      ref: "BookableUnitTypes",
     },
   ],
   reservations: [
