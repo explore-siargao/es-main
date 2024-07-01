f5e71fe1518bcca6d0d607b69e3b13e53e82f403
jeusnsn <jeusnsn@users.noreply.github.com>
2024-07-01 03:58:06 +0000

    Prettified Code!

--- packages/contract/src/Reservation/zod.ts
+++ packages/contract/src/Reservation/zod.ts
@@ -1,11 +1,17 @@
 import z from "zod"
-import { Z_Activity, Z_BookableUnitTypes, Z_PaymentMethod, Z_PersonalInfo, Z_Rental } from ".."
+import {
+  Z_Activity,
+  Z_BookableUnitTypes,
+  Z_PaymentMethod,
+  Z_PersonalInfo,
+  Z_Rental,
+} from ".."
 
 export const Z_Reservation = z.object({
   _id: z.string().optional(),
-  unitId:  z.union([Z_BookableUnitTypes, z.string()]).optional(),
+  unitId: z.union([Z_BookableUnitTypes, z.string()]).optional(),
   rentalId: z.union([Z_Rental, z.string()]).optional(),
-  activityId:  z.union([Z_Activity, z.string()]).optional(),
+  activityId: z.union([Z_Activity, z.string()]).optional(),
   guest: z.union([Z_PersonalInfo, z.string()]),
   paymentType: z.string().optional(),
   cardType: z.string().optional(),
@@ -15,14 +21,16 @@ export const Z_Reservation = z.object({
   xendItPaymentRequestId: z.string(),
   xendItPaymentReferenceId: z.string(),
   guestCount: z.number(),
-  status: z.enum([
-    "Confirmed",
-    "Not-Confirmed",
-    "Cancelled",
-    "Checked-In",
-    "Checked-Out",
-    "No-Show",
-  ]).optional(),
+  status: z
+    .enum([
+      "Confirmed",
+      "Not-Confirmed",
+      "Cancelled",
+      "Checked-In",
+      "Checked-Out",
+      "No-Show",
+    ])
+    .optional(),
   startDate: z.string().optional(),
   endDate: z.string().optional(),
   createdAt: z.string().optional(),
