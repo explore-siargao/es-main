2d3fb6d2b6ee6e264eb9293bfac7e8a1fdeaf605
JP Madrigal <jeusdsn@gmail.com>
2024-07-05 07:45:56 +0800

    feat(web): guide location guide ui

--- apps/web/module/BlogGuide/Guide/TravelBlog/TopThingsToDo.tsx
+++ apps/web/module/BlogGuide/Guide/TravelBlog/TopThingsToDo.tsx
@@ -3,29 +3,28 @@ import React from "react"
 function TopThingsToDo() {
   return (
     <div>
-      <h2 className="text-xl font-bold mb-2">TOP THINGS TO DO</h2>
+      <h2 className="text-xl font-bold mb-2">Top Things To Do</h2>
       <ul className="grid list-disc ml-16 space-y-5">
         <li>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Enjoy swimming in the natural pools with clear waters and unique rock formations during low tide.
         </li>
         <li>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Experience the thrill of cliff jumping into deeper sections of the pools (if you're adventurous and it's safe to do so).
         </li>
         <li>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Capture stunning shots of the rock pools, turquoise waters, and scenic coastal views through photography.
         </li>
         <li>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Discover diverse marine life such as fish and crabs in the tide pools while exploring.
+        </li>
+        <li>
+        Relax on the flat rocks surrounding the pools, sunbathe, and take in the natural beauty of the area.
         </li>
       </ul>
-      <h2 className="text-xl font-bold mt-10 mb-2">FOODIE NOTES</h2>
+      <h2 className="text-xl font-bold mt-10 mb-2">Foodie Notes</h2>
       <ul className="grid list-disc ml-16 space-y-5">
-        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+        <li> Try local specialties like grilled fish, seafood soups, or adobo (a popular Filipino dish with meat marinated and cooked in soy sauce, vinegar, and garlic).</li>
+        <li>Look out for vendors selling fresh tropical fruits like mangoes, pineapples, and bananas. They make for refreshing snacks and are widely available in the area..</li>
       </ul>
     </div>
   )
--- apps/web/module/BlogGuide/Guide/TravelBlog/TravelChecklist.tsx
+++ apps/web/module/BlogGuide/Guide/TravelBlog/TravelChecklist.tsx
@@ -3,43 +3,26 @@ import React from "react"
 function TravelChecklist() {
   return (
     <div>
-      <h2 className="text-xl font-bold mb-2">TRAVEL REQUIREMENTS CHECKLIST</h2>
+      <h2 className="text-xl font-bold mb-2">Travel Requirements Checklist</h2>
       <ul className="grid list-disc ml-6 space-y-5">
         <li>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris.
+        Vaccination card or vaccination certificate
         </li>
         <li>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris.
+        Confirmed roundtrip tickets
         </li>
         <li>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris.
+        Confirmed accredited accommodation booking for at least 2 nights
         </li>
         <li>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris.
+        Valid ID
         </li>
         <li>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris.
+        Surigao del Norte e-Health Pass
         </li>
         <li>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris.
-        </li>
-        <li>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris.
-        </li>
+        Approved Travel Coordination Permit (TCP) through S-Pass
+        </li>e
       </ul>
     </div>
   )
