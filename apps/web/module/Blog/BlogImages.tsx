7b51b3598f748f94604e0726eb5546b1765ed9ec
JP Madrigal <jeusdsn@gmail.com>
2024-07-05 08:46:19 +0800

    feat(web): blog general ui

---
+++ apps/web/module/Blog/BlogImages.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react"
+import data from "./data.json"
+import ImageGallery from "@/module/Accommodation/components/ImageGallery"
+import { Typography } from "@/common/components/ui/Typography"
+import ShareSave from "@/module/Accommodation/components/ShareSave"
+import ImageGalleryModal from "@/module/Accommodation/components/modals/ImageGalleryModal"
+
+function TravelBlog() {
+  const images = data.surfGuide.images
+  const [galleryModalOpen, setGalleryModalOpen] = useState(false)
+  const openModal = () => {
+    setGalleryModalOpen(true)
+  }
+
+  return (
+    <div>
+      <div className="justify-between md:flex text-start items-center">
+        <div>
+          <Typography variant="h1" fontWeight="semibold">
+          Siargao, dream island of the Philippines
+          </Typography>
+        </div>
+        <ShareSave />
+      </div>
+      <div className="flex flex-col gap-2 mb-8 mt-6">
+        <ImageGallery
+          images={images}
+          openModal={openModal}
+          isViewModal={true}
+        />
+      </div>
+      <h2 className="text-xl font-bold mb-2">About Blog</h2>
+      <p className="mb-8">A special atmosphere, with a lot of young people with a surfing spirit / 
+        good vibes and beautiful landscapes. Even before touching the ground, you will realize the 
+        beauty of the island. Indeed, from the air, you will see hundreds of thousands of palm trees, 
+        as far as the eye can see. There are many things to do in Siargao, that is what we will detail in this article.</p>
+      <ImageGalleryModal
+        images={images}
+        isOpen={galleryModalOpen}
+        onClose={() => setGalleryModalOpen(false)}
+      />
+    </div>
+  )
+}
+
+export default TravelBlog
---
+++ apps/web/module/Blog/WhereToStay.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+function WhereToStay() {
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-2">The Ideal Area To Stay</h2>
+      <div>If you want to be in the heart of the entertainment, enjoy island hopping,
+         restaurants and shops, we recommend you to stay at General Luna.
+          You will find many offers on Booking</div>
+      <h2 className="text-xl font-bold mt-10 mb-2">Good Plan For Accommodation</h2>
+      <div>We stayed in a quality Airbnb for 50€ per night. For the little story, we met the 
+        manager by chance who is none other than my former roommate in Austria! The world is really small.
+         For your comfort, we highly recommend this en-right for its good value for money. 
+         We were really happy to find this more upscale accommodation for a few nights.</div>
+    </div>
+  )
+}
+
+export default WhereToStay
