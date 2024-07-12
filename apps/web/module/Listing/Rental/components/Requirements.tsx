7f7000dad10b678ae020d264d9b96ebd6dda79d3
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-07-12 11:59:24 +0800

    feat(web): Connected available backend to rental public single page
    Prettified Code!

--- apps/web/module/Listing/Rental/components/Requirements.tsx
+++ apps/web/module/Listing/Rental/components/Requirements.tsx
@@ -3,19 +3,23 @@ import { Check } from "lucide-react"
 import { TitleSection } from "./TitleSection"
 
 const Requirements = ({ requirementData, group }: any) => {
-  const { minAgeReq, requiredDeposit } = requirementData
+  const { haveDriverLicense, requiredDeposit } = requirementData
   return (
     <>
       <TitleSection size="lg" title="Requirements">
         <div className="grid grid-cols-2">
-          {requiredDeposit ? (
+          {requiredDeposit && requiredDeposit !== 0 && (
             <div className="flex my-3">
               <Check className="text-primary-500 mr-4 " /> Down payment required
             </div>
-          ) : null}
+          )}
           <div className="flex my-3">
-            <Check className="text-primary-500 mr-4 " /> Minimum age required{" "}
-            {minAgeReq}
+            {haveDriverLicense === "Yes" && (
+              <>
+                <Check className="text-primary-500 mr-4" />
+                Driver License
+              </>
+            )}
           </div>
         </div>
       </TitleSection>
