ef31102d5634d0d7bcd6c20af008b3621037a7b5
jeusnsn <jeusnsn@users.noreply.github.com>
2024-08-22 06:59:22 +0000

    Prettified Code!

--- apps/web/module/Hosting/Reservations/Calendar/hooks/useGetCalendarWholePlace.ts
+++ apps/web/module/Hosting/Reservations/Calendar/hooks/useGetCalendarWholePlace.ts
@@ -2,7 +2,10 @@ import { API_URL_PROPERTIES } from "@/common/constants/api-routes"
 import { ApiService } from "@/common/service/api"
 import { useQuery } from "@tanstack/react-query"
 
-export async function getCalendarWholePlace(startDate: string, endDate: string) {
+export async function getCalendarWholePlace(
+  startDate: string,
+  endDate: string
+) {
   const apiService = new ApiService("v2")
   return await apiService.get(
     `${API_URL_PROPERTIES}/calendar/whole-place?startDate=${startDate}&endDate=${endDate}`
