cb44a5a284988f2a016a6554016176063be780a3
JP Madrigal <jeusdsn@gmail.com>
2024-08-22 16:01:30 +0800

    feat(web): connect calendar unit whole place reservation backend to frontend

---
+++ apps/web/module/Hosting/Reservations/Calendar/Property/WholePlaceReservationCalendar.tsx
@@ -0,0 +1,44 @@
+"use client"
+import { Typography } from "@/common/components/ui/Typography"
+import CalendarTable from "./WholePlaceCalendarTable"
+import { Table } from "lucide-react"
+import { Button } from "@/common/components/ui/Button"
+import { useRouter } from "next/navigation"
+import Tabs from "@/common/components/Tabs"
+import reservationCalendarTabs from "../../helpers/reservationCalendarTabs"
+
+const ReservationCalendar = () => {
+  const router = useRouter()
+  return (
+    <div className="mt-20">
+      <div className="mb-4">
+        <div className="flex-flex-col">
+          <div className="flex gap-2 items-center justify-between">
+            <Typography
+              variant="h1"
+              fontWeight="semibold"
+              className="flex justify-between items-center"
+            >
+              Reservations
+            </Typography>
+            <Button
+              onClick={() => router.push("/hosting/reservations/upcoming")}
+              variant={"primary"}
+              className="flex gap-2"
+            >
+              <Table />
+              Table View
+            </Button>
+          </div>
+          <Tabs tabs={reservationCalendarTabs} />
+        </div>
+
+        <div className="flex mt-2">
+          <CalendarTable />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ReservationCalendar
