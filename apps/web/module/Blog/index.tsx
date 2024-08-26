9cde2fcd4ba236a807ba65a81cecd5fc80b3d902
kinnethjavier <kinnethandrew9@gmail.com>
2024-08-26 15:09:18 +0800

    feat(web): Created general blog page and its CMS. Added some improvements in some guides pages.

--- apps/web/module/Blog/index.tsx
+++ apps/web/module/Blog/index.tsx
@@ -1,55 +1,94 @@
 "use client"
 import { WidthWrapper } from "@/common/components/WidthWrapper"
 import { useParams } from "next/navigation"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Separator } from "@/common/components/ui/Separator"
-import TopThingsToDo from "./TopThingsToDo"
-import data from "./data.json"
-import WhereToStay from "./WhereToStay"
 import BlogImages from "./BlogImages"
-import BlogChecklist from "./BlogChecklist"
 import PopularBlogs from "./PopularBlogs"
-function Blog() {
-  const params = useParams()
-  const travelName = params.travelName
-  const images = data.surfGuide.images
-  return (
-    <WidthWrapper width="small" className="mt-2">
-      <div className="py-8 grid lg:grid-cols-5">
-        <div className="lg:col-span-4 lg:mr-20 mr-10 space-y-10">
-          <BlogImages />
-          <Separator orientation="horizontal" className="my-10 bg-gray-300" />
-          <BlogChecklist />
-          <Separator orientation="horizontal" className="my-10 bg-gray-300" />
-          <TopThingsToDo />
-          <Separator orientation="horizontal" className="my-10 bg-gray-300" />
-          <WhereToStay />
-        </div>
+import { WEB_URL } from "@/common/constants/ev"
+import serialize from "../../common/components/RichText/serialize"
+import { Spinner } from "@/common/components/ui/Spinner"
+import { Typography } from "@/common/components/ui/Typography"
 
-        <div>
-          <PopularBlogs />
-          <div className="sticky top-2">
-            <div className="grid w-60">
+const BlogContent = ({ blogData }: { blogData: any }) => {
+  return (
+    <div>
+      <div className="mt-2">
+        <div className="py-8 flex">
+          <div className="lg:mr-20 mr-10 space-y-10 flex-1">
+            <BlogImages title={blogData.title} images={blogData.hero.images} />
+            <div className="prose max-w-full">
+              {serialize(blogData?.content?.mainContent)}
+            </div>
+          </div>
+
+          <div>
+            <PopularBlogs data={blogData?.sideContent.popularBlogs} />
+            <div className="flex w-60 flex-col sticky top-28">
               <div>
                 <h1 className="mt-10 text-xl font-bold">Page Summary</h1>
                 <Separator
                   orientation="horizontal"
                   className="mb-5 bg-gray-300"
                 />
-                <ul className="grid list-disc ml-5 space-y-5">
-                  <li>Introduction</li>
-                  <li>Travel Requirements Checklist</li>
-                  <li>Top Things To Do</li>
-                  <li>Foodie Notes</li>
-                  <li>Where To Stay</li>
+                <ul
+                  className="list-disc list-inside space-y-3 max-h-40 w-full overflow-y-auto pr-2"
+                  style={{
+                    scrollbarWidth: "none",
+                    msOverflowStyle: "none",
+                  }}
+                >
+                  {blogData.sideContent.pageSummary.map(
+                    (note: any, index: number) => (
+                      <li key={index}>{note.item}</li>
+                    )
+                  )}
                 </ul>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </WidthWrapper>
+    </div>
   )
+}
+
+function Blog() {
+  const params = useParams()
+  const blogName = params.blogName
+  const [blogData, setBlogData] = useState<any>([])
+  const [blogDataLoading, setBlogDataLoading] = useState(true)
+  const getBlogCms = async () => {
+    try {
+      const res = await fetch(`${WEB_URL}/cms/api/blogs/guide/${blogName}`)
+
+      if (!res.ok) {
+        throw new Error(`Response status: ${res.status}`)
+      }
+
+      const data = await res.json()
+      setBlogData(data.docs[0])
+      setBlogDataLoading(false)
+    } catch (err) {
+      setBlogDataLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getBlogCms()
+  }, [])
+
+  let content
+
+  if (blogDataLoading) {
+    content = <Spinner variant="primary" middle />
+  } else if (blogData) {
+    content = <BlogContent blogData={blogData} />
+  } else {
+    content = <Typography className="mt-10">No data was found.</Typography>
+  }
+
+  return <WidthWrapper width="medium">{content}</WidthWrapper>
 }
 
 export default Blog
