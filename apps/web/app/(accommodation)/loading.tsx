d2b3354dc0c3d7a7859e83d7b13bd4f64ee7609c
JP Madrigal <jp.madrigal07@gmail.com>
2024-08-31 14:26:55 +0800

    chore: Update loading component to use spinner animation

--- apps/web/app/(accommodation)/loading.tsx
+++ apps/web/app/(accommodation)/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-  return <span>Loading...</span>
+  return (
+    <div className="animate-spin w-4 h-4 border-2 border-current border-t-transparent text-primary-200 rounded-full">
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
 }
