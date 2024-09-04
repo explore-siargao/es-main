c10d37cfef75ecf7a09e90a82407b1b95d6bb136
ramil25 <ramil25@users.noreply.github.com>
2024-09-04 08:17:59 +0000

    Prettified Code!

--- apps/web/module/Hosting/Reservations/Calendar/hooks/useGetVehiclesByRentalId.ts
+++ apps/web/module/Hosting/Reservations/Calendar/hooks/useGetVehiclesByRentalId.ts
@@ -2,12 +2,12 @@ import { API_URL_RENTALS } from "@/common/constants/api-routes"
 import { ApiService } from "@/common/service/api"
 import { useQuery } from "@tanstack/react-query"
 
-export async function getVehiclesByRentalId(id:string) {
+export async function getVehiclesByRentalId(id: string) {
   const apiService = new ApiService()
   return await apiService.get(`${API_URL_RENTALS}/${id}/ids`)
 }
 
-function useGetVehiclesByRentalId(id:string) {
+function useGetVehiclesByRentalId(id: string) {
   const query = useQuery({
     queryKey: ["rental-vehicles-ids"],
     queryFn: () => getVehiclesByRentalId(id),
