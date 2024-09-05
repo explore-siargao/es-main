d5f880e8f3d8dda97a28f5047b3e6466bbb9691a
ramil25 <ramilkaharian25@gmail.com>
2024-09-05 13:43:02 +0800

    feat(web): Added custom hook for adding new rental reservation

---
+++ apps/web/module/Hosting/Reservations/Calendar/hooks/useAddRentalReservation.ts
@@ -0,0 +1,28 @@
+import { ApiService } from "@/common/service/api"
+import { API_URL_RESERVATIONS } from "@/common/constants"
+import { useMutation } from "@tanstack/react-query"
+
+type T_RentalReservation ={
+    start_date:string,
+    end_date:string,
+    status:string
+    name:string | null
+    guest_count: string | null
+    unit:string
+}
+export async function addRentalReservation(
+  props: T_RentalReservation
+) {
+  const apiService = new ApiService()
+  return await apiService.post(
+    `${API_URL_RESERVATIONS}/rental`,
+    props
+  )
+}
+function useAddRentalReservation() {
+  const query = useMutation({
+    mutationFn: (props: T_RentalReservation) => addRentalReservation(props),
+  })
+  return query
+}
+export default useAddRentalReservation
