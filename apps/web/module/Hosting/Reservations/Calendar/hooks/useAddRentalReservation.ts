0ce0c835b86ca93c8bce5c724f4b4e3388288635
ramil25 <ramil25@users.noreply.github.com>
2024-09-05 05:44:58 +0000

    Prettified Code!

--- apps/web/module/Hosting/Reservations/Calendar/hooks/useAddRentalReservation.ts
+++ apps/web/module/Hosting/Reservations/Calendar/hooks/useAddRentalReservation.ts
@@ -2,22 +2,17 @@ import { ApiService } from "@/common/service/api"
 import { API_URL_RESERVATIONS } from "@/common/constants"
 import { useMutation } from "@tanstack/react-query"
 
-type T_RentalReservation ={
-    start_date:string,
-    end_date:string,
-    status:string
-    name:string | null
-    guest_count: string | null
-    unit:string
+type T_RentalReservation = {
+  start_date: string
+  end_date: string
+  status: string
+  name: string | null
+  guest_count: string | null
+  unit: string
 }
-export async function addRentalReservation(
-  props: T_RentalReservation
-) {
+export async function addRentalReservation(props: T_RentalReservation) {
   const apiService = new ApiService()
-  return await apiService.post(
-    `${API_URL_RESERVATIONS}/rental`,
-    props
-  )
+  return await apiService.post(`${API_URL_RESERVATIONS}/rental`, props)
 }
 function useAddRentalReservation() {
   const query = useMutation({
