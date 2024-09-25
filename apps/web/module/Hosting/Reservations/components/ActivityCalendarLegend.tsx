c5a5edbee887a634c8b54e5af57f5d7f21d23dec
Dyayy026 <Dyayy026@users.noreply.github.com>
2024-09-25 06:31:11 +0000

    Prettified Code!

--- apps/web/module/Hosting/Reservations/components/ActivityCalendarLegend.tsx
+++ apps/web/module/Hosting/Reservations/components/ActivityCalendarLegend.tsx
@@ -10,32 +10,32 @@ const ActivityCalendarLegend = () => {
   const date = new Date()
 
   return (
-      <div className="flex items-center bg-white p-1">
-        <Button
-          variant="link"
-          className="text-base text-blue-600 hover:text-blue-600"
-          onClick={() => setLegendModalOpen(true)}
-        >
-          Expand Legends
-        </Button>
-        <button
-          className="flex items-center justify-end space-x-4 cursor-pointer"
-          onClick={() => setLegendModalOpen(true)}
-        >
-          {activitylegends.map((item) => (
-            <div className="flex items-center space-x-1.5" key={item.legend}>
-              <div className={`${item.color} h-4 w-4`} />
-              <Typography>{item.legend}</Typography>
-            </div>
-          ))}
-        </button>
+    <div className="flex items-center bg-white p-1">
+      <Button
+        variant="link"
+        className="text-base text-blue-600 hover:text-blue-600"
+        onClick={() => setLegendModalOpen(true)}
+      >
+        Expand Legends
+      </Button>
+      <button
+        className="flex items-center justify-end space-x-4 cursor-pointer"
+        onClick={() => setLegendModalOpen(true)}
+      >
+        {activitylegends.map((item) => (
+          <div className="flex items-center space-x-1.5" key={item.legend}>
+            <div className={`${item.color} h-4 w-4`} />
+            <Typography>{item.legend}</Typography>
+          </div>
+        ))}
+      </button>
 
-        <ActivityLegendModal
-          isOpen={legendModalOpen}
-          onClose={() => setLegendModalOpen(false)}
-          date={date}
-        />
-      </div>
+      <ActivityLegendModal
+        isOpen={legendModalOpen}
+        onClose={() => setLegendModalOpen(false)}
+        date={date}
+      />
+    </div>
   )
 }
 
