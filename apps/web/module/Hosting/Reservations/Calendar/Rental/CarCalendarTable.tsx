2e575d276bfce6a079b36b1965507683a4788bc1
JP Madrigal <jeusdsn@gmail.com>
2024-09-26 15:42:38 +0800

    feat(web): Correct the alignment on left of blocksbars on calendar

--- apps/web/module/Hosting/Reservations/Calendar/Rental/CarCalendarTable.tsx
+++ apps/web/module/Hosting/Reservations/Calendar/Rental/CarCalendarTable.tsx
@@ -270,15 +270,17 @@ const CarCalendarTable = () => {
       return null
     }
 
-    const startOffset = differenceInDays(bookingStart, addDays(startDate, -1))
+    const startOffset = isBefore(bookingStart, addDays(startDate, -1))
+    ? differenceInDays(bookingStart, addDays(startDate, -1)) - 0.5
+    : differenceInDays(bookingStart, addDays(startDate, -1));
     const endOffset = differenceInDays(bookingEnd, addDays(startDate, -1))
-
-    const startCol = Math.max(startOffset, 0)
-    const endCol = Math.min(endOffset, daysPerPage - 1)
+    const startCol = Math.max(startOffset, -.5)
+    const endCol = Math.min(endOffset, daysPerPage - .5)
 
     const colSpan = endCol - startCol + 1
     return { startCol, colSpan }
   }
+ 
 
   const handleEditRoom = (abbr: string) => {
     setEditingRoom(abbr)
--- apps/web/module/Hosting/Reservations/Calendar/Rental/MotorCalendarTable.tsx
+++ apps/web/module/Hosting/Reservations/Calendar/Rental/MotorCalendarTable.tsx
@@ -244,11 +244,13 @@ const MotorCalendarTable = () => {
       return null
     }
 
-    const startOffset = differenceInDays(bookingStart, addDays(startDate, -1))
+    const startOffset = isBefore(bookingStart, addDays(startDate, -1))
+    ? differenceInDays(bookingStart, addDays(startDate, -1)) - 0.5
+    : differenceInDays(bookingStart, addDays(startDate, -1));
     const endOffset = differenceInDays(bookingEnd, addDays(startDate, -1))
+    const startCol = Math.max(startOffset, -.5)
+    const endCol = Math.min(endOffset, daysPerPage - .5)
 
-    const startCol = Math.max(startOffset, 0)
-    const endCol = Math.min(endOffset, daysPerPage - 1)
     const colSpan = endCol - startCol + 1
     return { startCol, colSpan }
   }
