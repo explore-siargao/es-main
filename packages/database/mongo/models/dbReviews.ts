01688fc923560d398f16c2213f5dc52a61e659df
ramil25 <ramilkaharian25@gmail.com>
2024-10-28 11:44:31 +0800

    feat(api): Added collection schema for reviews

---
+++ packages/database/mongo/models/dbReviews.ts
@@ -0,0 +1,68 @@
+import mongoose from "mongoose"
+const { Schema } = mongoose
+
+const reviews = new Schema({
+  reviewerId: {
+    type: Schema.Types.ObjectId,
+    ref: "Guests",
+    required: true,
+  },
+  cleanlinessRates: {
+    type: Number,
+    required: true,
+  },
+  accuracyRates: {
+    type: Number,
+    required: true,
+  },
+  checkInRates: {
+    type: Number,
+    required: true,
+  },
+  communicationRates: {
+    type: Number,
+    required: true,
+  },
+  valueRates: {
+    type: Number,
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: false,
+  },
+  property: {
+    type: {
+      propertyId: {
+        type: Schema.Types.ObjectId,
+        ref: "Properties",
+      },
+      bookableUnitId: {
+        type: Schema.Types.ObjectId,
+        ref: "BookableUnitTypes",
+      },
+    },
+    required: false,
+    default: null,
+  },
+  rental: {
+    type: Schema.Types.ObjectId,
+    ref: "Rentals",
+    required: false,
+    default: null,
+  },
+  activity: {
+    type: Schema.Types.ObjectId,
+    ref: "Activities",
+    required: false,
+    default: null,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: Date,
+  deletedAt: Date,
+})
+
+export default mongoose.model("Reviews", reviews)
