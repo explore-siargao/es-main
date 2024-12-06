4690191241391b2ce99434c3603ca7c0b87991ab
ramil25 <ramilkaharian25@gmail.com>
2024-12-06 14:09:27 +0800

    feat(api): Added endpoint for paying reservations using manual input of card

--- apps/api/src/routes/api/reservations/services/cart-reservations.ts
+++ apps/api/src/routes/api/reservations/services/cart-reservations.ts
@@ -4,7 +4,7 @@ import {
 } from '@/common/constants'
 import { API_URL } from '@/common/constants/ev'
 import { ResponseService } from '@/common/service/response'
-import { T_CardInfo } from '@repo/contract'
+import { T_CardInfo, Z_CardInfo } from '@repo/contract'
 import {
   T_Add_To_Cart,
   T_Cart_Item,
@@ -133,6 +133,112 @@ export const cardMultipleCheckout = async (req: Request, res: Response) => {
         const cardInfo = encryptionService.decrypt(
           paymentMethod?.cardInfo as string
         ) as T_CardInfo
+        const cardResponse = await fetch(
+          `${API_URL}/api/v1/xendit/card-payment`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              amount: amount,
+              cardNumber: cardInfo.cardNumber,
+              expirationMonth: cardInfo.expirationMonth,
+              expirationYear: cardInfo.expirationYear,
+              cardHolderName: cardInfo.cardholderName,
+              country: cardInfo.country,
+              cvv: cardInfo.cvv,
+              customer: customer,
+              userId: userId,
+            }),
+          }
+        )
+
+        const cardData = await cardResponse.json()
+        if (cardData.item.actions) {
+          const reservationItems = cartItems.map((item) => ({
+            activityIds: item.activityIds || null,
+            rentalIds: item.rentalIds || null,
+            propertyIds: item.propertyIds || null,
+            startDate: item.startDate,
+            endDate: item.endDate,
+            guest: userId,
+            xendItPaymentMethodId: cardData.item.payment_method.id,
+            xendItPaymentRequestId: cardData.item.id,
+            xendItPaymentReferenceId: cardData.item.reference_id,
+            guestCount: item.guestCount,
+            status: 'For-Payment',
+            cartId: item.id,
+          }))
+          const addReservations =
+            await dbReservations.insertMany(reservationItems)
+
+          if (addReservations) {
+            res.json(
+              response.success({
+                item: {
+                  reservations: addReservations,
+                  action: {
+                    type: 'PAYMENT',
+                    link: cardData.item.actions[0].url,
+                  },
+                  message: 'Pending payment',
+                },
+              })
+            )
+          } else {
+            res.json(response.error({ message: 'Invalid card details' }))
+          }
+        } else {
+          res.json(
+            response.error({
+              message: cardData.item.message || UNKNOWN_ERROR_OCCURRED,
+            })
+          )
+        }
+      }
+    }
+  } catch (err: any) {
+    res.json(
+      response.error({
+        message: err.message
+          ? err.message + ' ' + err.stack
+          : UNKNOWN_ERROR_OCCURRED,
+      })
+    )
+  }
+}
+
+export const manualCardMultipleCheckout = async (req: Request, res: Response) => {
+  try {
+    const userId = res.locals.user?.id
+    const customer = res.locals.user.personalInfo
+    const cardInfo:T_CardInfo = req.body.cardInfo
+    const cartItems: T_Add_To_Cart[] = req.body.cartItems
+    const validCardInfo = Z_CardInfo.safeParse(cardInfo)
+    if(validCardInfo.error){
+      console.error(validCardInfo.error)
+      res.json(response.error({message:REQUIRED_VALUE_EMPTY}))
+    }
+    if (!cartItems || cartItems.length === 0 || !cardInfo) {
+      res.json(response.error({ message: REQUIRED_VALUE_EMPTY }))
+    } else if (!Array.isArray(cartItems)) {
+      res.json(response.error({ message: 'Invalid Item on cart' }))
+    } else {
+      const parseCartItems = Z_Add_CartItems.safeParse(cartItems)
+      if (!parseCartItems.success) {
+        res.json(
+          response.error({
+            items: parseCartItems.error.errors,
+            message: 'Invalid Item on cart',
+          })
+        )
+      } else {
+        const amount = cartItems.reduce(
+          (total, item) => total + (item.price || 0),
+          0
+        )
+       
         const cardResponse = await fetch(
           `${API_URL}/api/v1/xendit/card-payment`,
           {
---
+++ apps/web/module/Cart/hooks/use-add-manual-card-payment.ts
@@ -0,0 +1,17 @@
+import { T_Add_To_Cart } from "@repo/contract-2/cart"
+import { ReservationService } from "@repo/contract-2/reservations"
+import { useMutation } from "@tanstack/react-query"
+
+export async function addCardPayment(props: T_Add_To_Cart[]) {
+  const reservation = new ReservationService()
+  return await reservation.addItem(props, "manual")
+}
+
+function useAddManualCardPayment() {
+  const query = useMutation({
+    mutationFn: (props: T_Add_To_Cart[]) => addCardPayment(props),
+  })
+  return query
+}
+
+export default useAddManualCardPayment
