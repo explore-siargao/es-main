5c1d459ab463101bf6e7853351bf1fc3a7dcb3fa
JP Madrigal <jp.madrigal07@gmail.com>
2024-12-21 05:13:04 +0800

    feat: Added rental pickup dropoff calendar function and ui

--- apps/web/module/Listing/Rental/checkout-box.tsx
+++ apps/web/module/Listing/Rental/checkout-box.tsx
@@ -12,15 +12,35 @@ import { T_Rental } from "@repo/contract-2/rental"
 import { GUEST_COMMISSION_PERCENT } from "@repo/constants"
 import PriceBreakdownModal from "./modals/price-breakdown-modal"
 import usePickupDropoffStore from "./stores/use-pickup-dropoff-store"
+import { Option, Select } from "@/common/components/ui/Select"
+import CheckInOutModal from "./modals/check-in-out-modal"
+import { T_Add_To_Cart } from "@repo/contract-2/cart"
+import useAddToCart from "@/common/hooks/use-add-to-cart"
+import { useQueryClient } from "@tanstack/react-query"
+import toast from "react-hot-toast"
+import combineDateTime from "./helpers/combine-date-time"
 
 const CheckoutBox = ({ rental }: { rental: T_Rental }) => {
   const router = useRouter()
+  const queryClient = useQueryClient()  
   const params = useParams<{ listingId: string }>()
+  const [checkInOutCalendarModalIsOpen, setCheckInOutCalendarModalIsOpen] =
+    useState(false)
   const [isBreakdownModalOpen, setIsBreakdownModalOpen] = useState(false)
   const [isMoreInfoModalOpen, setIsMoreInfoModalOpen] = useState(false)
+  const { mutate: addToCart, isPending: isAddToCartPending } = useAddToCart()
   const dateRange = usePickupDropoffStore((state) => state.dateRange)
-  const updateDateRange = usePickupDropoffStore(
-    (state) => state.updateDateRange
+  const fromTime = usePickupDropoffStore(
+    (state) => state.fromTime
+  )
+  const toTime = usePickupDropoffStore(
+    (state) => state.toTime
+  )
+  const updateFromTime = usePickupDropoffStore(
+    (state) => state.updateFromTime
+  )
+  const updateToTime = usePickupDropoffStore(
+    (state) => state.updateToTime
   )
   const daysCount = differenceInDays(
     dateRange.to ?? new Date(),
@@ -31,79 +51,132 @@ const CheckoutBox = ({ rental }: { rental: T_Rental }) => {
   const esCommissionTotal = dayRateTotal * GUEST_COMMISSION_PERCENT || 0
   const totalBeforeTaxes = dayRateTotal + esCommissionTotal || 0
 
-  const generateLocalDateTime = ({ date }: { date: Date }) => {
-    const now = new Date(date || "")
-    const year = now.getFullYear()
-    const month = String(now.getMonth() + 1).padStart(2, "0")
-    const day = String(now.getDate()).padStart(2, "0")
-    const hours = String(now.getHours()).padStart(2, "0")
-    const minutes = String(now.getMinutes()).padStart(2, "0")
-    return `${year}-${month}-${day}T${hours}:${minutes}`
+  const handleAddToCartSingleItem = () => {
+    const from = combineDateTime(dateRange.from!, fromTime)
+    const to = combineDateTime(dateRange.to!, fromTime)
+    const payload: T_Add_To_Cart = {
+      price: totalBeforeTaxes,
+      rentalIds: { rentalId: rental._id, qtyIdsId: rental.qtyIds![0]?._id },
+      startDate: from?.toISOString() || "",
+      endDate: to?.toISOString() || "",
+      guestCount: 1,
+    }
+    addToCart(payload, {
+      onSuccess: (data) => {
+        if (!data.error) {
+          queryClient.invalidateQueries({
+            queryKey: ["get-cart-item"],
+          })
+          toast.success((data.message as string) || "Added to cart", {
+            duration: 5000,
+          })
+        } else {
+          toast.error(String(data.message))
+        }
+      },
+      onError: (err) => {
+        toast.error(String(err))
+      },
+    })
   }
 
   return (
     <div className="border rounded-xl shadow-lg px-6 pb-6 pt-5 flex flex-col divide-text-100 overflow-y-auto mb-5">
       <Typography variant="h2" fontWeight="semibold" className="mb-4">
         {formatCurrency(dayRate)}
-        <small className="font-light"> day</small>
+        <small className="font-light"> per day</small>
       </Typography>
       <div className="font-semibold grid grid-cols-1 gap-3 w-full">
-        <div className="grid grid-cols-1 gap-3">
-          <div className="relative rounded-xl px-3 pb-1.5 pt-2.5 ring-1 ring-inset ring-text-200 focus-within:z-10 focus-within:ring-2 focus-within:ring-text-600 hover:cursor-pointer">
+        <div className="flex gap-4">
+          <div
+            className="flex-1 relative rounded-xl px-3 pb-1.5 pt-2.5 ring-1 ring-inset ring-text-200 focus-within:z-10 focus-within:ring-2 focus-within:ring-text-600 hover:cursor-pointer"
+            onClick={() =>
+              setCheckInOutCalendarModalIsOpen(!checkInOutCalendarModalIsOpen)
+            }
+          >
             <label
               htmlFor="check-in"
               className="block text-xs font-medium text-text-900 hover:cursor-pointer"
             >
-              Pick-up date and time <Asterisk />
+              Pick-up date <Asterisk />
             </label>
-            <input
-              className="w-full border-transparent text-sm p-0"
-              type="datetime-local"
-              min={format(new Date(), "yyyy-MM-dd'T'HH:mm")}
-              max={format(
-                subDays(new Date(dateRange.to || ""), 1),
-                "yyyy-MM-dd'T'HH:mm"
-              )}
-              defaultValue={generateLocalDateTime({
-                date: dateRange.from || new Date(),
-              })}
-              onChange={(e) =>
-                updateDateRange({
-                  from: new Date(e.target.value),
-                  to: new Date(dateRange.to || ""),
-                })
-              }
-            />
+            <span className="block w-full border-0 p-0 text-text-900 placeholder:text-text-400 focus:ring-0 sm:text-sm sm:leading-6 bg-transparent disabled:opacity-50">
+              {dateRange.from
+                ? format(dateRange.from, "MM/dd/yyyy")
+                : "Add date"}
+            </span>
           </div>
+          <Select
+            label={"Pick-up time"}
+            id="testable"
+            className="flex-1"
+            onChange={(e) =>
+              updateFromTime(e.target.value)
+            }
+            defaultValue={fromTime}
+            required
+          >
+            {Array.from({ length: ((22 - 6) * 2) + 1 }).map((_, index) => {
+              const hours = Math.floor((6 * 60 + index * 30) / 60);
+              const minutes = (index * 30) % 60;
+              const timeString = `${hours % 12 || 12}:${minutes
+                .toString()
+                .padStart(2, "0")} ${hours >= 12 ? "PM" : "AM"}`; // AM/PM format
+              return (
+                <Option key={timeString} value={timeString}>
+                  {timeString}
+                </Option>
+              );
+            })}
+          </Select>
         </div>
-        <div className="grid grid-cols-1 gap-3">
-          <div className="relative rounded-xl px-3 pb-1.5 pt-2.5 ring-1 ring-inset ring-text-200 focus-within:z-10 focus-within:ring-2 focus-within:ring-text-600 hover:cursor-pointer">
+        <div className="flex gap-4">
+          <div
+            className="flex-1 relative rounded-xl px-3 pb-1.5 pt-2.5 ring-1 ring-inset ring-text-200 focus-within:z-10 focus-within:ring-2 focus-within:ring-text-600 hover:cursor-pointer"
+            onClick={() =>
+              setCheckInOutCalendarModalIsOpen(!checkInOutCalendarModalIsOpen)
+            }
+          >
             <label
               htmlFor="check-in"
               className="block text-xs font-medium text-text-900 hover:cursor-pointer"
             >
-              Drop-off date and time <Asterisk />
+              Drop-off date <Asterisk />
             </label>
-            <input
-              className="w-full border-transparent text-sm p-0"
-              type="datetime-local"
-              min={format(new Date(dateRange.from || ""), "yyyy-MM-dd'T'HH:mm")}
-              defaultValue={generateLocalDateTime({
-                date: dateRange.to || new Date(),
-              })}
-              value={format(new Date(dateRange.to || ""), "yyyy-MM-dd'T'HH:mm")}
-              onChange={(e) =>
-                updateDateRange({
-                  from: dateRange.from,
-                  to: new Date(e.target.value),
-                })
-              }
-            />
+            <span className="block w-full border-0 p-0 text-text-900 placeholder:text-text-400 focus:ring-0 sm:text-sm sm:leading-6 bg-transparent disabled:opacity-50">
+              {dateRange.to
+                ? format(dateRange.to, "MM/dd/yyyy")
+                : "Add date"}
+            </span>
           </div>
+          <Select
+            label={"Drop-off time"}
+            id="testable"
+            className="flex-1"
+            onChange={(e) => updateToTime(e.target.value)}
+            defaultValue={toTime}
+            required
+          >
+            {Array.from({ length: ((22 - 6) * 2) + 1 }).map((_, index) => {
+              const hours = Math.floor((6 * 60 + index * 30) / 60);
+              const minutes = (index * 30) % 60;
+              const timeString = `${hours % 12 || 12}:${minutes
+                .toString()
+                .padStart(2, "0")} ${hours >= 12 ? "PM" : "AM"}`; // AM/PM format
+
+              // Use timeString for both value and label
+              return (
+                <Option key={timeString} value={timeString}>
+                  {timeString}
+                </Option>
+              );
+            })}
+          </Select>
         </div>
         <Button
           variant="primary"
           className="font-bold"
+          disabled={!dateRange.from || !dateRange.to || !fromTime || !toTime || isAddToCartPending}
           onClick={() =>
             router.push(`/accommodation/${params.listingId}/checkout`)
           }
@@ -113,7 +186,8 @@ const CheckoutBox = ({ rental }: { rental: T_Rental }) => {
         <Button
           variant="default"
           className="font-bold"
-          onClick={() => console.log("add to cart")}
+          disabled={!dateRange.from || !dateRange.to || !fromTime || !toTime || isAddToCartPending}
+          onClick={() => handleAddToCartSingleItem()}
         >
           <LucideShoppingCart size={20} className="mr-2" /> Add to cart
         </Button>
@@ -154,6 +228,10 @@ const CheckoutBox = ({ rental }: { rental: T_Rental }) => {
         dayRate={dayRate}
         dayRateTotal={dayRateTotal}
         dateRange={dateRange}
+      />
+      <CheckInOutModal
+        isOpen={checkInOutCalendarModalIsOpen}
+        onClose={() => setCheckInOutCalendarModalIsOpen(false)}
       />
       <CheckoutMoreInfoModal
         isOpen={isMoreInfoModalOpen}
--- apps/web/module/Listing/Rental/stores/use-pickup-dropoff-store.ts
+++ apps/web/module/Listing/Rental/stores/use-pickup-dropoff-store.ts
@@ -2,10 +2,12 @@ import { create } from "zustand"
 import { DateRange } from "react-day-picker"
 import { addHours } from "date-fns"
 
-type T_DateRange = { dateRange: DateRange }
+type T_DateRange = { dateRange: DateRange, fromTime: string, toTime: string }
 
 type T_DateRange_Action = {
   updateDateRange: (dateRange: DateRange) => void
+  updateFromTime: (fromTime: string) => void
+  updateToTime: (toTime: string) => void
 }
 
 const today = addHours(new Date(), 1)
@@ -17,11 +19,25 @@ const usePickupDropoffStore = create<T_DateRange & T_DateRange_Action>(
       from: addHours(new Date(), 1),
       to: futureDate,
     },
+    fromTime: "6:00 AM",
+    toTime: "6:00 PM",
     updateDateRange: (dateRange: DateRange) =>
       set((state: T_DateRange & T_DateRange_Action) => ({
         ...state,
         dateRange: { ...dateRange },
       })),
+    updateFromTime: (fromTime: string) => {
+      set((state: T_DateRange & T_DateRange_Action) => ({
+        ...state,
+        fromTime,
+      }))
+    },
+    updateToTime: (toTime: string) => {
+      set((state: T_DateRange & T_DateRange_Action) => ({
+        ...state,
+        toTime,
+      }))
+    }
   })
 )
 
