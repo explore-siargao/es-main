7d2007b9db33496b53a85405fc958f322f84980b
JP Madrigal <jp.madrigal07@gmail.com>
2024-12-25 09:00:32 +0800

    chore: update property filtered title to unit title

--- apps/api/src/routes/api/properties/services/filtered.ts
+++ apps/api/src/routes/api/properties/services/filtered.ts
@@ -797,7 +797,7 @@ export const getFilteredProperties = async (req: Request, res: Response) => {
         const allBookableUnits = changePrices.flatMap((property: T_Property) =>
           property.bookableUnits.map((unit: T_BookableUnitType) => ({
             listingId: property._id,
-            title: property.title || null,
+            title: unit.title || null,
             subtitle: unit.subtitle || null,
             type: property.type,
             wholePlaceType: unit.wholePlaceType,
@@ -1486,7 +1486,7 @@ export const getFilteredProperties = async (req: Request, res: Response) => {
         const allBookableUnits = changePrices.flatMap((property: T_Property) =>
           property.bookableUnits.map((unit: T_BookableUnitType) => ({
             listingId: property._id,
-            title: property.title || null,
+            title: unit.title || null,
             subtitle: unit.subtitle || null,
             type: property.type,
             wholePlaceType: unit.wholePlaceType,
@@ -2205,7 +2205,7 @@ export const getFilteredProperties = async (req: Request, res: Response) => {
         const allBookableUnits = changePrices.flatMap((property: T_Property) =>
           property.bookableUnits.map((unit: T_BookableUnitType) => ({
             listingId: property._id,
-            title: property.title || null,
+            title: unit.title || null,
             subtitle: unit.subtitle || null,
             type: property.type,
             wholePlaceType: unit.wholePlaceType,
@@ -2933,7 +2933,7 @@ export const getFilteredProperties = async (req: Request, res: Response) => {
         const allBookableUnits = changePrices.flatMap((property: T_Property) =>
           property.bookableUnits.map((unit: T_BookableUnitType) => ({
             listingId: property._id,
-            title: property.title || null,
+            title: unit.title || null,
             subtitle: unit.subtitle || null,
             type: property.type,
             wholePlaceType: unit.wholePlaceType,
@@ -2951,7 +2951,7 @@ export const getFilteredProperties = async (req: Request, res: Response) => {
             reviewsCount: unit.reviewsCount || 0,
           }))
         )
-
+        
         const paginatedBookableUnits = allBookableUnits.slice(
           startIndex,
           endIndex
--- apps/web/module/Listing/Property/host-policies.tsx
+++ apps/web/module/Listing/Property/host-policies.tsx
@@ -5,12 +5,17 @@ import { T_Property } from "@repo/contract-2/property"
 
 const HostPolicies = ({ property }: { property: T_Property }) => {
   const thingsToKnow = property.policies?.filter((policy) => policy.category === "Things To Know")
+  const safetyConsiderations = property.policies?.filter((policy) => policy.category === "Safety Considerations")
+  const safetyDevices = property.policies?.filter((policy) => policy.category === "Safety Devices")
+  const propertyInformation = property.policies?.filter((policy) => policy.category === "Property Information")
+  const additionalRules = property.policies?.filter((policy) => policy.category === "Additional Rules")
   return (
     <div>
       <Typography variant="h3" fontWeight="semibold">
         Host policies
       </Typography>
-      <div className="mt-6 grid grid-cols-4">
+      <div className="mt-6 grid grid-cols-3 gap-y-6 gap-x-4">
+      {thingsToKnow && thingsToKnow.length > 0 ? (
         <div>
           <Typography variant="h4" fontWeight="semibold" className="flex gap-2">
             <LucideLightbulb /> Things to know
@@ -21,82 +26,66 @@ const HostPolicies = ({ property }: { property: T_Property }) => {
                 {policy.policy}
               </li>
             )) : null}
-            {thingsToKnow && thingsToKnow.length > 0 ? thingsToKnow.map((policy) => (
-              <li>
-                {policy.policy}
-              </li>
-            )) : null}
           </ul>
         </div>
+        ) : null}
+        {safetyConsiderations && safetyConsiderations.length > 0 ? (
         <div>
           <Typography variant="h4" fontWeight="semibold" className="flex gap-2">
             <LucideShield /> Safety considerations
           </Typography>
           <ul className="ml-6 mt-4 text-text-400 list-decimal">
-            {thingsToKnow && thingsToKnow.length > 0 ? thingsToKnow.map((policy) => (
-              <li>
-                {policy.policy}
-              </li>
-            )) : null}
-            {thingsToKnow && thingsToKnow.length > 0 ? thingsToKnow.map((policy) => (
+            {safetyConsiderations && safetyConsiderations.length > 0 ? safetyConsiderations.map((policy) => (
               <li>
                 {policy.policy}
               </li>
             )) : null}
           </ul>
         </div>
+        ) : null}
+        {safetyDevices && safetyDevices.length > 0 ? (
         <div>
           <Typography variant="h4" fontWeight="semibold" className="flex gap-2">
             <LucideMonitorSmartphone /> Safety devices
           </Typography>
           <ul className="ml-6 mt-4 text-text-400 list-decimal">
-            {thingsToKnow && thingsToKnow.length > 0 ? thingsToKnow.map((policy) => (
-              <li>
-                {policy.policy}
-              </li>
-            )) : null}
-            {thingsToKnow && thingsToKnow.length > 0 ? thingsToKnow.map((policy) => (
+            {safetyDevices && safetyDevices.length > 0 ? safetyDevices.map((policy) => (
               <li>
                 {policy.policy}
               </li>
             )) : null}
           </ul>
         </div>
+        ) : null}
+        {propertyInformation && propertyInformation.length > 0 ? (
         <div>
           <Typography variant="h4" fontWeight="semibold" className="flex gap-2">
             <LucideInfo /> Property information
           </Typography>
           <ul className="ml-6 mt-4 text-text-400 list-decimal">
-            {thingsToKnow && thingsToKnow.length > 0 ? thingsToKnow.map((policy) => (
-              <li>
-                {policy.policy}
-              </li>
-            )) : null}
-            {thingsToKnow && thingsToKnow.length > 0 ? thingsToKnow.map((policy) => (
+            {propertyInformation && propertyInformation.length > 0 ? propertyInformation.map((policy) => (
               <li>
                 {policy.policy}
               </li>
             )) : null}
           </ul>
         </div>
-      </div>
-      <div className="mt-6">
+        ) : null}
+        {additionalRules && additionalRules.length > 0 ? (
+        <div>
           <Typography variant="h4" fontWeight="semibold" className="flex gap-2">
             <LucideScale /> Additional rules
           </Typography>
           <ul className="ml-6 mt-4 text-text-400 list-decimal">
-            {thingsToKnow && thingsToKnow.length > 0 ? thingsToKnow.map((policy) => (
-              <li>
-                {policy.policy}
-              </li>
-            )) : null}
-            {thingsToKnow && thingsToKnow.length > 0 ? thingsToKnow.map((policy) => (
+            {additionalRules && additionalRules.length > 0 ? additionalRules.map((policy) => (
               <li>
                 {policy.policy}
               </li>
             )) : null}
           </ul>
         </div>
+        ) : null}
+      </div>
     </div>
   )
 }
