0da83bfc2a4d2e3c931412f8ea06c1a7b0e8c3d8
ramil25 <ramilkaharian25@gmail.com>
2025-01-28 11:10:45 +0800

    feat(api):created seeder for host approvals

--- apps/api/src/routes/api/host-approvals/services/default.ts
+++ apps/api/src/routes/api/host-approvals/services/default.ts
@@ -1,6 +1,7 @@
 import { UNKNOWN_ERROR_OCCURRED } from '@/common/constants'
 import { FileService } from '@/common/service/file'
 import { ResponseService } from '@/common/service/response'
+import { Z_Add_Host_Approval } from '@repo/contract-2/host-approval'
 import { dbHostApproval } from '@repo/database'
 import { Request, Response } from 'express'
 const response = new ResponseService()
@@ -13,6 +14,17 @@ export const addHostApproval = async (req: Request, res: Response) => {
   try {
     console.log(files)
     const upload = await fileService.upload({ files })
+
+    const validHostApproval = Z_Add_Host_Approval.safeParse({
+      businessType:businessType,
+      companyName:companyName,
+      brn:brn,
+      registeredAddress:registeredAddress,
+      photocopyBusinessPermit:JSON.stringify(
+        { fileKey: upload.key, createdAt: new Date() },
+      ),
+    })
+    if(validHostApproval.success){
     const newHostApproval = new dbHostApproval({
       userId,
       businessType,
@@ -21,6 +33,7 @@ export const addHostApproval = async (req: Request, res: Response) => {
       registeredAddress,
       createdAt: Date.now(),
     })
+    
     await newHostApproval.save()
     await dbHostApproval.findByIdAndUpdate(newHostApproval._id, {
       $set: {
@@ -35,6 +48,10 @@ export const addHostApproval = async (req: Request, res: Response) => {
         message: 'Host approval request has been submitted',
       })
     )
+  }else{
+    console.error(validHostApproval.error.message)
+    res.json(response.error({message:"Invalid payload"}))
+  }
   } catch (err: any) {
     res.json(
       response.error({
---
+++ packages/seeder/src/host-approval.ts
@@ -0,0 +1,75 @@
+import { dbHostApproval, dbUsers } from "@repo/database";
+
+const seedHostApprovals = async()=>{
+    console.log("Seeding host approvals...")
+    try {
+        const host = await dbUsers.findOne({role: "Host"})
+         await dbHostApproval.insertMany([
+            {
+                businessType:"Hotel",
+                companyName:"Hotel One",
+                brn:"123-456-789",
+                registeredAddress:"Siargao City",
+                userId: host?._id,
+                photocopyBusinessPermit:{
+                    fileKey:"f204e5e6-525b-4631-9287-ab8dd943b092",
+                    createdAt: new Date()
+                },
+                createdAt: new Date()
+            },
+            {
+                businessType:"Apartment",
+                companyName:"Apartment One",
+                brn:"123-456-780",
+                registeredAddress:"Siargao City",
+                userId: host?._id,
+                photocopyBusinessPermit:{
+                    fileKey:"f204e5e6-525b-4631-9287-ab8dd943b092",
+                    createdAt: new Date()
+                },
+                createdAt: new Date()
+            },
+            {
+                businessType:"Hostel",
+                companyName:"Hostel One",
+                brn:"123-456-785",
+                registeredAddress:"Siargao City",
+                userId: host?._id,
+                photocopyBusinessPermit:{
+                    fileKey:"f204e5e6-525b-4631-9287-ab8dd943b092",
+                    createdAt: new Date()
+                },
+                createdAt: new Date()
+            },
+            {
+                businessType:"Resort",
+                companyName:"Resort One",
+                brn:"123-456-787",
+                registeredAddress:"Siargao City",
+                userId: host?._id,
+                photocopyBusinessPermit:{
+                    fileKey:"f204e5e6-525b-4631-9287-ab8dd943b092",
+                    createdAt: new Date()
+                },
+                createdAt: new Date()
+            },
+            {
+                businessType:"Rentals Vehicles",
+                companyName:"Rentals One",
+                brn:"123-456-782",
+                registeredAddress:"Siargao City",
+                userId: host?._id,
+                photocopyBusinessPermit:{
+                    fileKey:"f204e5e6-525b-4631-9287-ab8dd943b092",
+                    createdAt: new Date()
+                },
+                createdAt: new Date()
+            },
+        ])
+        console.log("Host approvals seeded successfully")
+    } catch (err:any) {
+        console.error("Error seeding host approvals, ", err)
+    }
+}
+
+export default seedHostApprovals;
